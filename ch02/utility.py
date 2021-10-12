4a31a51e2e62e62f79a6e2c4601fbcc17e49df4d
Cody Sun <553589030@qq.com>
2021-10-12 11:24:41 +0800

    修改文件目录

---
+++ ch02/utility.py
@@ -0,0 +1,13 @@
+from jpype import JClass
+from pyhanlp import HanLP
+
+
+def load_dictionary():
+    """
+    加载HanLP中的mini词库
+    :return: 一个set形式的词库
+    """
+    IOUtil = JClass('com.hankcs.hanlp.corpus.io.IOUtil')
+    path = HanLP.Config.CoreDictionaryPath.replace('.txt', '.mini.txt')
+    dic = IOUtil.loadDictionary([path])
+    return set(dic.keySet())
--- hanlp_study/ch02/speed_benchmark.py
+++
@@ -1,9 +0,0 @@
-import time
-
-
-def evaluate_speed(segment, text, dic, pressure):
-    start_time = time.time()
-    for i in range(pressure):
-        segment(text, dic)
-    elapsed_time = time.time() - start_time
-    print('%.2f 万字/秒' % (len(text) * pressure / 10000 / elapsed_time))
